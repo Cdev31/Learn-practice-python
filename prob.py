164b8eb596669d7debb0b6b0efea1b38d3842b2d
kalet chavez <kalet.svdev@gmail.com>
2023-02-12 16:22:07 -0600

    segundo commit

--- prob.py
+++ prob.py
@@ -1,8 +1,13 @@
 
 print("hello world")
 
-def suma(num1,num2):
-   return num1 + num2
+def suma(num1,num2,num3):
+   return num1 + num2 + num3
 
-print(suma(3,4))
+print(suma(5,4))
 
+num1 = int(input("agrege el primer numero"))
+num2 = int(input("agrege el segundo numero"))   
+num3 = int(input("agrege el tercer numero"))   
+
+print(f"su suma es de {suma(num1,num2,num3)}")
